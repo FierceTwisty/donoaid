6ff5082ffbd87d83f5b9e660aa8e329b65344668
Anirbaan Ghatak <anirbaan.ghatak1.ag@gmail.com>
2021-09-14 01:22:16 +0530

    Login and SignUp 1.0

---
+++ lib/auth_frb.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import './auth_form.dart';
+
+class AuthFrb extends StatefulWidget {
+  @override
+  _AuthFrbState createState() => _AuthFrbState();
+}
+
+class _AuthFrbState extends State<AuthFrb> {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  var _isLoading = false;
+
+  void _submitAuthForm(
+    String email,
+    String password,
+    String username,
+    bool isLogin,
+    BuildContext ctx,
+  ) async {
+    try {
+      // UserCredential userCred;
+      setState(() {
+        _isLoading = true;
+      });
+      if (isLogin) {
+        await _auth.signInWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+      } else {
+        await _auth.createUserWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+
+//----------------------------------------------------------------------------//
+
+//this is to be used for adding image to the cloud.........
+
+//        final ref = FirebaseStorage.instance
+//            .ref()
+//            .child('user_image')
+//            .child(authResult.user.uid + '.jpg');
+//
+//        await ref.putFile(image).onComplete;
+//
+//        final url = await ref.getDownloadURL();
+
+//----------------------------------------------------------------------------//
+
+        // await Firestore.instance
+        //     .collection('users')
+        //     .document(authResult.user.uid)
+        //     .setData({
+        //   'username': username,
+        //   'email': email,
+        // });
+      }
+    } on FirebaseAuthException catch (e) {
+      var msg;
+
+      if (e.code == 'weak-password') {
+        msg = 'The password provided is too weak.';
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        msg = 'The account already exists for that email.';
+        print('The account already exists for that email.');
+      }
+      if (e.code == 'user-not-found') {
+        msg = 'No user found for that email.';
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        msg = 'Wrong password provided for that user.';
+        print('Wrong password provided for that user.');
+      }
+
+      final snackBar = SnackBar(content: Text(msg), backgroundColor: Colors.red,);
+
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      setState(() {
+        _isLoading = false;
+      });
+    } catch (err) {
+      print(err);
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200], //change thru colorRGB
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            // color: Colors.grey[100], //change thru colorRGB
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'DonoAid',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 40,
+                    fontWeight: FontWeight.w600,
+                    fontFamily: 'Raleway',
+                    ),
+              ),
+            ),
+          ),
+          AuthForm(
+            _submitAuthForm,
+            _isLoading,
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,56 @@
-import 'package:donoaid/home.dart';
+// import 'package:donoaid/home.dart';
 import 'package:flutter/material.dart';
+import 'auth_frb.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  _MyAppState createState() => _MyAppState();
+}
+
+// This widget is the root of your application.
+class _MyAppState extends State<MyApp> {
+  // @override
+  bool _initialized = false;
+  bool _error = false;
+
+  void initializeFlutterFire() async {
+    try {
+      await Firebase.initializeApp();
+
+      setState(() {
+        _initialized = true;
+      });
+    } catch (e) {
+      setState(() {
+        _error = true;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    initializeFlutterFire();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (_error) {
+      return Text("Shit went wrong");
+    }
+    if (!_initialized) {
+      return CircularProgressIndicator();
+    }
+
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: Home(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: Colors.blue),
+      home: AuthFrb(),
     );
   }
 }
-
